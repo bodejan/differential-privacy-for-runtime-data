6d4e15d34eb97b3c93e62f5ab9c9f32193388854
Cedric Kring <cedric.kring@gmail.com>
2023-07-17 14:49:41 +0200

    visualize evaluation metrics

--- dashboard/regression_app.py
+++ dashboard/regression_app.py
@@ -19,38 +19,19 @@ def regression_layout():
                         dbc.CardBody([
                             html.H4("Evaluation using Regression Methods"),
                             html.Div([
-                                html.Label('Select dataset'),
-                                dcc.Dropdown(
-                                    id='optimizer',
-                                    options=[
-                                        {'label': 'RMSprop', 'value': 'RMSprop'},
-                                        {'label': 'Adagrad', 'value': 'Adagrad'},
-                                        {'label': 'Adam', 'value': 'adam'},
-                                        {'label': 'SGD', 'value': 'sgd'}
-                                    ],
-                                    value='adam'
-                                )
-                            ]),
-                            html.P("Select Test/Train Split"),
-                            dcc.Slider(30, 90, 5, value=70, id='split'),
-                            html.P("Select Epochs"),
-                            dcc.Slider(1, 90, 5, value=20, id='epochs'),
-                            html.Br(),
-                            html.Div([
                                 dcc.Loading(id="loading3", type="circle",
                                             children=[
                                                 html.Button('Submit', id='regbutton', n_clicks=0,
                                                             style={'align': 'center', 'width': '100%',
                                                                    'display': 'inline-block',
                                                                    'background-color': '#4CAF50', 'color': 'white'}),
-                                                html.Div(id='outputreg')
                                             ])])
                         ]))], width=4),
                 dbc.Col([
                     dbc.Card(
                         dbc.CardBody([
                             html.H4("Prediction Quality:"),
-                            html.Div(id="results"),
+                            dash_table.DataTable(id='regression_result', data=[], page_size=10),
                         ])),
                 ], width=8),
             ], style={'height': '15cm'})])
@@ -59,15 +40,17 @@ def regression_layout():
 
 def regression_callbacks(app):
     @app.callback(
-        [dash.dependencies.Output('outputreg', 'children'),
-         ],
+        [
+            dash.dependencies.Output('regression_result', 'data'),
+        ],
         [dash.dependencies.Input('regbutton', 'n_clicks')],
-        [dash.dependencies.State('dataset', 'value'),
-         dash.dependencies.State('session-id', 'children'),
-         ], prevent_initial_call=True)
-    def update_output(n_clicks, dataset, uuid: list[str]):
+        [dash.dependencies.State('session-id', 'children')],
+        prevent_initial_call=True
+    )
+    def update_output(n_clicks, uuid: list[str]):
         session_id = uuid[0]
         meta = MetaInformation.from_id_file(session_id)
         print("Regression")
-        return ["Result =" + str(
-            eval_reg.Regression.eval_input_data(input_file=f'temp/{session_id}.csv', dataset_name=meta.dataset_name))]
+
+        result = eval_reg.Regression.eval_input_data(input_file=f'temp/{session_id}.csv', dataset_name=meta.dataset_name)
+        return [result]
--- dashboard/training_app.py
+++ dashboard/training_app.py
@@ -1,4 +1,3 @@
-
 import dash
 from dash.dependencies import Output, Input, State
 import dash_bootstrap_components as dbc
@@ -10,64 +9,72 @@ import pandas as pd
 
 def training_layout():
     layout = html.Div(
-    [
-        html.Br(),
-        dbc.Row([
-        dbc.Col([
-        dbc.Card(
-            dbc.CardBody([
-                html.H4("Training of NN"),
+        [
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card(
+                        dbc.CardBody([
+                            html.H4("Training of NN"),
 
-                html.Div([
-                    html.Label('Select Optimizer'),
-                    dcc.Dropdown(
-                        id='optimizer',
-                        options=[
-                            {'label': 'RMSprop', 'value': 'RMSprop'},
-                            {'label': 'Adagrad', 'value': 'Adagrad'},
-                            {'label': 'Adam', 'value': 'adam'},
-                            {'label': 'SGD', 'value': 'sgd'}
-                        ],
-                        value='adam'
-                    )
-                ]),
-                html.P("Select Test/Train Split"),
-                dcc.Slider(30, 90, 5, value=70, id='split'),
-                html.P("Select Epochs"),
-                dcc.Slider(1, 90, 5, value=20, id='epochs'),
-                html.Br(),
-                html.Div([
-                    dcc.Loading(id="loading2" ,type="circle",
-                                children=[
-                                    html.Button('Submit', id='nnbutton', n_clicks=0, style={'align': 'center', 'width' :'100%', 'display': 'inline-block', 'background-color': '#4CAF50', 'color': 'white'}),
-                                    html.Div(id='outputnn')
-                                ])])
-            ], style={'margin' :"0.5cm"}))
-    ], width=4),
-    dbc.Col([
-        dbc.Card(
-            dbc.CardBody([
-                html.H4("Prediction Quality:"),
-                dcc.Graph(id="eval_fig"),
-                dcc.Graph(id="eval_fig2"),
-                html.Div(id="metrics"),
-            ])),
-    ], width=8),
-    ], style={'height' :'15cm'})])
+                            html.Div([
+                                html.Label('Select Optimizer'),
+                                dcc.Dropdown(
+                                    id='optimizer',
+                                    options=[
+                                        {'label': 'RMSprop', 'value': 'RMSprop'},
+                                        {'label': 'Adagrad', 'value': 'Adagrad'},
+                                        {'label': 'Adam', 'value': 'adam'},
+                                        {'label': 'SGD', 'value': 'sgd'}
+                                    ],
+                                    value='adam'
+                                )
+                            ]),
+                            html.P("Select Test/Train Split"),
+                            dcc.Slider(30, 90, 5, value=70, id='split'),
+                            html.Div([
+                                html.Label('Epochs'),
+                                dbc.Input(id="nn_epochs", type="number", value=10, min=10, max=100000, step=10),
+                            ]),
+                            html.Br(),
+                            html.Div([
+                                dcc.Loading(id="loading2", type="circle",
+                                            children=[
+                                                html.Button('Submit', id='nnbutton', n_clicks=0,
+                                                            style={'align': 'center', 'width': '100%',
+                                                                   'display': 'inline-block',
+                                                                   'background-color': '#4CAF50', 'color': 'white'}),
+                                                html.Div(id='outputnn')
+                                            ])])
+                        ], style={'margin': "0.5cm"}))
+                ], width=4),
+                dbc.Col([
+                    dbc.Card(
+                        dbc.CardBody([
+                            html.H4("Evaluation Metrics"),
+                            dash_table.DataTable(id='nn_metrics', data=[], page_size=10),
+                            html.Br(),
+                            html.H4("Prediction Quality:"),
+                            dcc.Graph(id="eval_fig"),
+                            dcc.Graph(id="eval_fig2"),
+                        ])),
+                ], width=8),
+            ], style={'height': '15cm'})])
     return layout
+
 
 def training_callbacks(app):
     @app.callback(
         [dash.dependencies.Output('outputnn', 'children'),
          dash.dependencies.Output('eval_fig', 'figure'),
          dash.dependencies.Output('eval_fig2', 'figure'),
-         dash.dependencies.Output('nnbutton', 'n_clicks'), dash.dependencies.Output('metrics', 'children')
+         dash.dependencies.Output('nn_metrics', 'data')
          ],
         [dash.dependencies.Input('nnbutton', 'n_clicks')],
         [dash.dependencies.State('dataset', 'value'),
          dash.dependencies.State('optimizer', 'value'),
          dash.dependencies.State('split', 'value'),
-         dash.dependencies.State('epochs', 'value'),
+         dash.dependencies.State('nn_epochs', 'value'),
          dash.dependencies.State('session-id', 'children'),
 
          ], prevent_initial_call=True)
@@ -77,5 +84,12 @@ def training_callbacks(app):
             return "Please enter a value and click the submit button.", n_clicks
         else:
             nn = eval_nn.NN()
-            fig1, fig2, mse1, mse2, mse3, mse4, mse5, mse6 = nn.train(dataset, uuid[0], optimizer, split, epochs, n_clicks)
-            return f"NN Trained", fig1, fig2, n_clicks, f'MSE (original): {mse1}, MSE (synthetic):{mse2}, MAE(original):{mse3}, MAE (synthetic): {mse4}, MAPE (original): {mse5}, MAPE (synthetic): {mse6}'
+            fig1, fig2, mse_original, mse_synthetic, mae_original, mae_synthetic, mape_original, mape_synthetic = nn.train(
+                dataset, uuid[0], optimizer, split, epochs, n_clicks)
+
+            data = [
+                {'Name': 'MSE', 'Original': mse_original, 'Synthetic': mse_synthetic},
+                {'Name': 'MAE', 'Original': mae_original, 'Synthetic': mae_synthetic},
+                {'Name': 'MAPE', 'Original': mape_original, 'Synthetic': mape_synthetic},
+            ]
+            return f"NN Trained", fig1, fig2, data
